880486d0047646ca073506094f3cf002d2b342cd
Sam Niyazi <sam2003@niyazi.com>
2020-09-23 10:21:52 -0500

    Completed M07-03

---
+++ WPF.Sample.DataLayer/EntityClasses/UserFeedback.cs
@@ -0,0 +1,91 @@
+﻿using Common.Library;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+// 09/23/2020 08:23 am - SSN - [20200923-0823] - [001] - M07-03 - Demo: Entity Framework classes and validation 
+
+namespace WPF.Sample.DataLayer
+{
+    [Table("UserFeedback")]
+    public class UserFeedback : CommonBase
+    {
+
+        #region Properties
+
+        private int _UserFeedbackId;
+
+        [Required]
+        [Key]
+        public int UserFeedbackId
+        {
+            get { return _UserFeedbackId; }
+            set
+            {
+                _UserFeedbackId = value;
+                RaisePropertyChanged("UserFeedbackId");
+
+            }
+        }
+
+        private string _Name;
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                _Name = value;
+                RaisePropertyChanged("Name");
+
+            }
+        }
+
+
+        private string _EmailAddress;
+
+        [Required(ErrorMessage = "Email address is required")]
+        public string EmailAddress
+        {
+            get { return _EmailAddress; }
+            set
+            {
+                _EmailAddress = value;
+                RaisePropertyChanged("EmailAddress");
+
+            }
+        }
+
+
+        private string _PhoneExtension;
+
+        public string PhoneExtension
+        {
+            get { return _PhoneExtension; }
+            set
+            {
+                _PhoneExtension = value;
+                RaisePropertyChanged("PhoneExtension");
+
+            }
+        }
+
+
+        private string _Message;
+
+        [Required(ErrorMessage = "Input is required")]
+        public string Message
+        {
+            get { return _Message; }
+            set
+            {
+                _Message = value;
+                RaisePropertyChanged("Message");
+
+            }
+        }
+
+        #endregion Properties
+
+    }
+}
--- WPF.Sample.DataLayer/Models/SampleDbContext.cs
+++ WPF.Sample.DataLayer/Models/SampleDbContext.cs
@@ -1,13 +1,43 @@
-﻿using System.Data.Entity;
+﻿using Common.Library;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
 
 namespace WPF.Sample.DataLayer
 {
-  public partial class SampleDbContext : DbContext
-  {
-    public SampleDbContext() : base("name=Samples")
+    public partial class SampleDbContext : DbContext
     {
+        public SampleDbContext() : base("name=Samples")
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<UserFeedback> UserFeedback { get; set; }
+
+
+
+        // 09/23/2020 10:14 am - SSN - [20200923-0823] - [002] - M07-03 - Demo: Entity Framework classes and validation 
+
+        public List<ValidationMessage> CreateValidationMessages(DbEntityValidationException ex)
+        {
+            List<ValidationMessage> ret = new List<ValidationMessage>();
+
+            foreach (DbValidationError error in ex.EntityValidationErrors.SelectMany(x => x.ValidationErrors))
+            {
+                ret.Add(new ValidationMessage
+                {
+                    Message = error.ErrorMessage,
+                    PropertyName = error.PropertyName
+                });
+
+            }
+
+            return ret;
+        }
+
+
     }
 
-    public virtual DbSet<User> Users { get; set; }
-  }
+
 }
